a27dcf029ddc99f62aaf5e41a9fe86fc02927b40
Obed Lukusa <obedlukusa28@gmail.com>
2026-09-24 22:51:52 +0100

    Fix base path pour GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 import {defineConfig} from 'vite';
 
+export default defineConfig({
+  base: '/wide/',
+  // ...le reste de votre config
+})
+
 export default defineConfig(() => {
   return {
     base: './',
